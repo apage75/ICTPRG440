c15ff7093d6dbb03b0839e41126202c805036161
apage75 <apage75@icloud.com>
2025-10-04 12:17:48 +1000

    Update main.py

--- src_code/main.py
+++ src_code/main.py
@@ -1,58 +1,45 @@
-#Programmed by Alison Page 9/9/25
- 
-import geopandas as gpd #gpd is the convention used to shorten the phrase geopandas.
- 
-FILE_PATH = r"C:\Programming\CorrectionalFacility\Correctional Facility.shp" #Global Variable set for input file location.
-OUTPUT_FOLDER_PATH = r"C:\Programming\Output" #Global variable set for Output folderpath
-print(FILE_PATH) #print file path location
- 
- 
-def transformFromWgs84ToMGA2020Z56(filePath, EPSG=4283): # Coordinate System is GDA 94
- 
-    if isinstance(filePath,str):
-        print("Your filepath is formatted as a string!\n")
+""" Programmed by Alison Page  September 2025 for tafe Unit ICTPRG440 - Assessment 2"""
+import geopandas as gpd
+
+FILE_PATH = r"C:\Programming\CorrectionalFacility\Correctional Facility.shp" #Global Variable set for input file location. code r refers to a raw string
+OUTPUT_FOLDER_PATH = r"C:\Programming\GitHub\ICTPRG440\src_code\Output" #Global variable set for Output folderpath
+
+#print(FILE_PATH) #prints file path location
+#print(OUTPUT_FOLDER_PATH) #prints file path location
+
+# read the file
+
+
+def transformFromWgs84ToMGA2020Z56(FilePath, EPSG=4326): 
+    """This function transforms the coordinators from WGS84 to another coordinate system currently set to WGS84"""
+
+    if isinstance(FilePath,str): # check to ensure the file path is formatted as a string
+            print("FILEPATH TYPE: Your filepath if formatted as a string")
     else:
-        raise ValueError ("Error - you need to enter the file path as a string\n")
-   
-    if isinstance(EPSG,int):
-        print("Your Coordinate System code is an integer!\n") # not working
+        raise ValueError("You need to enter the file path as a string")
+    
+    if isinstance(EPSG,int): # check to ensure the coordinate system code is entered as an iteger
+            print("EPSG: Your coordinate system is entered as an integer")
     else:
-        raise ValueError ("Error - you need to enter the ESPG as an integer\n")
-   
+        raise ValueError("You need to enter an integer")
+
     try:
-        gdf = gpd.read_file(filePath) #request to read the file
-        print(gdf.crs) #print coordinate reference system - current system is GDA 94
-        gdf = gdf.to_crs(epsg=EPSG) #default is set in the function - can be changed via dafault value to alter to a seperate coodrinate system when needed.
-        #print(gdf.head)
-     
+        gdf = gpd.read_file(FilePath) #function will return a geodataframe type that helps deal with geospatial data. 
+        gdf = gdf.to_crs(epsg=EPSG)#redefining the Coordinate reference system to Projected Coordinate System GDA 2020 MGA 56
+        
+        #print (type(gdf))
+        #print(gdf.crs) #checking the coordinate system that the data is created in
+
+        #print(gdf.head()) #prints first 5 rows ofthe geodataframe - similar to the attribute table in Arc. 
+        #print(gdf.columns) # prints the column header descriptors of the shape file. 
     except Exception as e:
-        print("**************************************ERROR***************************************************\n", e) # Error coded for printing out to the console in case there is an issue.
-   
-    return gdf #returns geodataframe object.
-   
-    print(type, gdf)
- 
-def exportGdfToGeoJson(gdf,OUTPUT_FOLDER_PATH): #geodataframe passed to the output file
-   
-        """This function is creating an output file as a string the file name created is output.GeoJson.
-        Nothing else is returned
-       
-        """
-       
-        isGdf = isinstance(gdf,gpd.geodataframe)
-        print(f"You have entered a Geodataframe")
-        isGdf != isinstance(gdf,gpd.geodataframe)
-        print(f"You have not entered a Geodataframe")
- 
-        fulloutputPathWithFileName = OUTPUT_FOLDER_PATH + "\\" + "output.GeoJson"   #using Driver GeoJson
-        gdf.to_file(fulloutputPathWithFileName, driver="GeoJSON")
-   
-        return None
- 
-gdftransformed = transformFromWgs84ToMGA2020Z56(FILE_PATH,EPSG=7856) #creates a variable to transform the Coordinate system to MGA2020Z56
- 
-exportGdfToGeoJson(gdftransformed,OUTPUT_FOLDER_PATH)
- 
-# Now we need a loop to show the spatial data, attribute table, row by row in the console. using a python looping method.
- 
- 
+        print("Error......................................................................\n", e)
+    
+    return gdf #returns the geodataframe object
+
+transformGdf = transformFromWgs84ToMGA2020Z56(FILE_PATH, EPSG=7856)
+"""Enter the cordinate system that you need the results returned as in this section"""
+
+print(transformGdf.head()) # prints first 5 rows of the geodataframe
+
+
